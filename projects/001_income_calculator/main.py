b9dab9b64872e5060d70f3734665364417d571e8
SreeVardhanTulluri <tulluris@in.glory-global.com>
2024-09-04 10:13:20 +0000

    Completed Project 1 & 2

--- projects/001_income_calculator/main.py
+++ projects/001_income_calculator/main.py
@@ -1,11 +1,16 @@
 # 1. Create a function to calculate finances
-def calculate_finances(monthly_income: float, tax_rate: float, currency: str) -> None:
+def calculate_finances(monthly_income: float, tax_rate: float, currency: str,other_expenses: tuple = ()) -> None:
     # 2. Do the math for each field
-    yearly_salary: float = monthly_income * 12
     monthly_tax: float = monthly_income * (tax_rate / 100)
-    yearly_tax: float = monthly_tax * 12
     monthly_net_income: float = monthly_income - monthly_tax
+    print(other_expenses)
+    monthly_expenses: float = sum(other_expenses)
+    monthly_balance: float = monthly_net_income-monthly_expenses
+    yearly_salary: float = monthly_income * 12
+    yearly_tax: float = monthly_tax * 12
     yearly_net_income: float = yearly_salary - yearly_tax
+    yearly_expenses: float = monthly_expenses*12
+    yearly_balance: float = yearly_net_income-yearly_expenses
 
     # 3. Format the information nicely for the user
     print('--------------------------------')
@@ -13,21 +18,49 @@ def calculate_finances(monthly_income: float, tax_rate: float, currency: str) ->
     print(f'Tax rate: {tax_rate:,.0f}%')
     print(f'Monthly tax: {currency}{monthly_tax:,.2f}')
     print(f'Monthly net income: {currency}{monthly_net_income:,.2f}')
+    print(f'Monthly expenses: {currency}{monthly_expenses:,.2f}')
+    print(f'Monthly balance: {currency}{monthly_balance:,.2f}')
     print(f'Yearly salary: {currency}{yearly_salary:,.2f}')
     print(f'Yearly tax paid: {currency}{yearly_tax:,.2f}')
     print(f'Yearly net income: {currency}{yearly_net_income:,.2f}')
+    print(f'Yearly expenses: {currency}{yearly_expenses:,.2f}')
+    print(f'Yearly balance: {currency}{yearly_balance:,.2f}')
     print('--------------------------------')
 
 
 # 4. Create a main entry point for the program
 def main() -> None:
     # 5. Gather user input
-    monthly_income: float = float(input('Enter your monthly income: '))
-    tax_rate: float = float(input('Enter your tax rate (%): '))
+    # monthly_income: float = float(input('Enter your monthly income: '))
+    # tax_rate: float = float(input('Enter your tax rate (%): '))
+    monthly_income: float = float_input('Enter your monthly income: ')
+    tax_rate: float = float_input('Enter your tax rate (%): ')
+    other_expenses: dict = dict()
+    choice:str = input('Wanna add Expenses ? (Y\\N) ')
+    if choice.lower() == 'y' or choice.lower() == 'yes':
+        while(True):
+            expense_type:str = input('Enter the expense type or Enter to Finish...\n')
+            if len(expense_type) != 0:
+                expense = float_input('Enter expense per month : ')
+                other_expenses[expense_type] = expense
+                expense_type = ''
+            else:
+                break
+        
 
     # 6. Call the function
-    calculate_finances(monthly_income, tax_rate, currency='KR')
+    calculate_finances(monthly_income, tax_rate, currency='KR',other_expenses=other_expenses.values())
 
+def float_input(*argv) ->float :
+    while(True):
+        try:
+            opr = float(input(argv[0]))
+            break
+            
+        except:
+            print('Invalid input')
+    
+    return opr
 
 # 7. Run the script
 if __name__ == "__main__":
@@ -35,9 +68,9 @@ if __name__ == "__main__":
 
 """
 Homework:
-1. Edit the script so that users can also enter their expenses (eg. rent, food, gym memberships) so they
+DONE:1. Edit the script so that users can also enter their expenses (eg. rent, food, gym memberships) so they
 can see how much they have left over each month.
-2. Recreate the user input section to safely handle users inserting the wrong type without 
+DONE:2. Recreate the user input section to safely handle users inserting the wrong type without 
 crashing the program.
 
 """
--- projects/002_expense_splitter/main.py
+++ projects/002_expense_splitter/main.py
@@ -1,32 +1,60 @@
 
 # 1. Create the function
-def calculate_split(total_amount: float, number_of_people: int, currency: str) -> None:
+def calculate_split(total_amount: float, number_of_people: int, currency: str,share_data:list = []) -> None:
     # 2. Validate the amount of people
     if number_of_people < 1:
         raise ValueError('Number of people must be greater than one.')
-
+    if len(share_data) != number_of_people:
+        print('Something went wrong while splitting by share...\nSplitting even Now...\n')
+        share_data = [100/number_of_people for _ in range(number_of_people)]
     # 3. Perform the calculation
-    share_per_person: float = total_amount / number_of_people
+    # share_per_person: float = total_amount / number_of_people
 
     # 4. Format the results and display them
     print(f'Total expense: {currency}{total_amount:,.2f}')
     print(f'Number of people: {number_of_people}')
-    print(f'Each person should pay: {currency}{share_per_person:,.2f}')
+    # print(f'Each person should pay: {currency}{share_per_person:,.2f}')
+    for i in range(number_of_people):
+        print(f'Share of person {i}: {currency}{total_amount*share_data[i]/100:,.2f}')
+        
+
 
 
 # 5. Create a main entry point
 def main() -> None:
     # 6. Try to get the user input and perform the calculation
     try:
-        total_amount: float = float(input('Enter the total amount of the expense: '))
+        total_amount: float = float_input('Enter the total amount of the expense: ')
         number_of_people: int = int(input('Enter the number of people sharing the expense: '))
+        share_data:list = list()
+        choice:str = input('Wanna add Expenses ? (Y\\N) ')
+        if choice.lower() == 'y' or choice.lower() == 'yes':
+            while True:
+                share_data:list = list()
+                for _ in range(number_of_people):
+                    share_data.append(float_input('Enter share : '))
+                if sum(share_data) == 100:
+                    break
+                else:
+                    print('Please re-enter share data or press Enter to Continue without share... \n')
+                    share_data = []
 
         # Call the function to calculate and display expenses
-        calculate_split(total_amount, number_of_people, currency='€')
+        calculate_split(total_amount, number_of_people, currency='€',share_data=share_data)
 
     except ValueError as e:
         print(f'Error: {e}')
 
+def float_input(*argv) ->float :
+    while(True):
+        try:
+            opr = float(input(argv[0]))
+            break
+            
+        except:
+            print('Invalid input')
+    
+    return opr
 
 # 7. Run the script
 if __name__ == '__main__':
